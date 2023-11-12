bddab4a58864d3d9c01048657f873f6c107d3ef8
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-11-12 15:21:53 +0100

    impement product detail feature

--- Rocky/Controllers/HomeController.cs
+++ Rocky/Controllers/HomeController.cs
@@ -1,26 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Rocky.Data;
 using Rocky.Models;
-using System;
-using System.Collections.Generic;
+using Rocky.Models.ViewModels;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Rocky.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly RockyDbContext _db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(RockyDbContext db,
+                              ILogger<HomeController> logger)
         {
+            _db = db;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeVM = new HomeVM
+            {
+                Products = _db.Product.Include(x => x.Category),
+                Categories = _db.Category
+            };
+
+            return View(homeVM);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var detailsVM = new DetailsVM
+            {
+                Product = _db.Product.Include(x => x.Category).Where(x => x.Id == id).FirstOrDefault(),
+                ExistsCart = false
+            };
+
+            return View(detailsVM);
         }
 
         public IActionResult Privacy()
