da40dd7f742a69fd752923678e9bdc441a88ac95
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-11-16 14:21:25 +0100

    add scaffold identity ui

--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -66,6 +66,7 @@ namespace Rocky
             app.UseSession();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
